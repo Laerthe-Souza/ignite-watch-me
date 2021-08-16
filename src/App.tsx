00ea61cc8d2d523f40b656404855c72ecfefe1c0
Laerthe Souza <laerte_souza.developer@outlook.com>
2021-08-15 23:32:01 -0300

    Perfomando a aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
 
 import './styles/global.scss';
+import { useCallback } from 'react';
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  function handleSelectGenre(id: number) {
+  const handleSelectGenre = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  }, []);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
